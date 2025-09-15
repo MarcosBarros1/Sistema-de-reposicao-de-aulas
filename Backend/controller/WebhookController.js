3552eb34cc73cff625b3bf1761e3714b8376ef1b
Rafael Maciel <rafaelmn934@gmail.com>
2025-09-15 20:16:46 -0300

    Testes de monitoramento: teste 01

--- Backend/controller/WebhookController.js
+++ Backend/controller/WebhookController.js
@@ -2,27 +2,29 @@
 const ReposicaoService = require('../services/ReposicaoService');
 
 class WebhookController {
-  async handleGoogleForm(req, res) {
-    try {
+    async handleGoogleForm(req, res) {
+        try {
 
-      const { id_solicitacao, matricula_aluno, concorda } = req.body;
+            console.log('>>> Webhook do Google Form recebido! Corpo da requisição:', req.body);
 
-      if (id_solicitacao === undefined || matricula_aluno === undefined || concorda === undefined) {
-        return res.status(400).json({ message: 'Dados insuficientes.' });
-      }
+            const { id_solicitacao, matricula_aluno, concorda } = req.body;
 
-      // Passamos os dados para o serviço
-      await ReposicaoService.registrarAssinatura({ 
-        idSolicitacao: id_solicitacao, 
-        matriculaAluno: matricula_aluno, 
-        concorda: concorda 
-      });
-      
-      res.status(200).json({ message: 'Webhook recebido com sucesso.' });
-    } catch (error) {
-      console.error('Erro no webhook do Google Form:', error);
-      res.status(500).json({ message: 'Erro interno no servidor.' });
+            if (id_solicitacao === undefined || matricula_aluno === undefined || concorda === undefined) {
+                return res.status(400).json({ message: 'Dados insuficientes.' });
+            }
+
+            // Passamos os dados para o serviço
+            await ReposicaoService.registrarAssinatura({
+                idSolicitacao: id_solicitacao,
+                matriculaAluno: matricula_aluno,
+                concorda: concorda
+            });
+
+            res.status(200).json({ message: 'Webhook recebido com sucesso.' });
+        } catch (error) {
+            console.error('Erro no webhook do Google Form:', error);
+            res.status(500).json({ message: 'Erro interno no servidor.' });
+        }
     }
-  }
 }
 module.exports = new WebhookController();
