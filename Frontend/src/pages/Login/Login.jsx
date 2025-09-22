b8dca37c8025296be1bbc2b12f12b2c3cb08b8d9
Rafael Maciel <rafaelmn934@gmail.com>
2025-09-22 17:54:44 -0300

    Corrigindo estilização das páginas de início e de login

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import SideBanner from '../../components/SideBanner/SideBanner';
-import './Login.css'; // Criaremos este arquivo para o layout
+import './Login.css';
 
 const Login = () => {
     const { tipo_usuario } = useParams();
 
     return (
         <div className="login-page-container">
-            <div className="side-banner-container">
-                <SideBanner />
-            </div>
+            <SideBanner />
             <div className="login-form-area">
                 <LoginForm tipo_usuario={tipo_usuario} />
             </div>
