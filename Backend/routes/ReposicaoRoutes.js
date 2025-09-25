d7418b10fb94c809ef2b9e1afa3bcbd755eb0790
Rafael Maciel <rafaelmn934@gmail.com>
2025-09-25 10:37:49 -0300

    Adicionando lógica de confirmação de aulas

--- Backend/routes/ReposicaoRoutes.js
+++ Backend/routes/ReposicaoRoutes.js
@@ -3,6 +3,7 @@ const ReposicaoController = require('../controller/ReposicaoController');
 const router = express.Router();
 
 // Rotas Específicas
+router.get('/autorizadas', ReposicaoController.listar_autorizadas);
 router.get('/pendentes-aprovacao', ReposicaoController.listar_pendentes);
 
 // Rotas Principais
@@ -13,5 +14,6 @@ router.get('/', ReposicaoController.listarTodos);
 router.get('/:id_solicitacao', ReposicaoController.buscarPorId);
 router.put('/:id_solicitacao/status', ReposicaoController.atualizarStatus); // Padronizado
 router.get('/:id_solicitacao/assinaturas', ReposicaoController.buscarAssinaturas); // Padronizado
+router.post('/:id_solicitacao/confirmar-realizacao', ReposicaoController.confirmar_realizacao);
 
 module.exports = router;
